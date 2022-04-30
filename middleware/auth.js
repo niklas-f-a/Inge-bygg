a819d75faf84a6e88c2ae3e616af0b4aaf1b389a
Linda Marko <lindamarko83@gmail.com>
2022-04-30 11:30:32 +0200

    Add timestamps to taskSchema to update updatedAt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,32 +1,32 @@
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
-const {Forbidden} = require('../error')
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { Forbidden } = require('../error');
 
 const verify = (header, role) => {
-  const token = header.replace('Bearer ', '')
-  const user = jwt.verify(token, process.env.JWT_SECRET)
-  if(user.role != role){
-    throw new Forbidden
+  const token = header.replace('Bearer ', '');
+  const user = jwt.verify(token, process.env.JWT_SECRET);
+  if (user.role != role) {
+    throw new Forbidden();
   }
-  return user
-}
+  return user;
+};
 
 module.exports = {
-  client(req, res, next){
-    const user = verify(req.headers.authorization, 'client')
-    req.user = user
-    next()
+  client(req, res, next) {
+    const user = verify(req.headers.authorization, 'client');
+    req.user = user;
+    next();
   },
 
-  admin(req, res, next){
-    const user = verify(req.headers.authorization, 'admin')
-    req.user = user
-    next()
+  admin(req, res, next) {
+    const user = verify(req.headers.authorization, 'admin');
+    req.user = user;
+    next();
   },
 
-  worker(req, res, next){
-    const user = verify(req.headers.authorization, 'worker')
-    req.user = user
-    next()
-  }
-}
+  worker(req, res, next) {
+    const user = verify(req.headers.authorization, 'worker');
+    req.user = user;
+    next();
+  },
+};
