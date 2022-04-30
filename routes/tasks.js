ee6f4934e47c88453d4327f2dcb3427ab4511a18
Linda Marko <lindamarko83@gmail.com>
2022-04-30 09:45:18 +0200

    Add tasks routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,15 @@
-const {Router} = require('express')
-const router = new Router()
-const auth = require('../middleware/auth')
+const { Router } = require('express');
+const router = new Router();
+const TaskController = require('../controllers/TaskController');
+const Auth = require('../middleware/auth');
 //authenticate user depending on role
 
+router.get('/', Auth.admin, TaskController.getAll);
 
+router.get('/:id', Auth.admin, TaskController.getTask);
 
+router.delete('/:id', Auth.admin, TaskController.delete);
 
+router.patch('/:id', Auth.worker, TaskController.update);
 
-
-module.exports = router
+module.exports = router;
