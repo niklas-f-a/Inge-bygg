c07e96f558b48ce978f8d79f799301f034437f00
Niklas Andersson <niklas.andersson@iths.se>
2022-05-04 16:11:56 +0200

    Change response in 404

--- routes/404.js
+++ routes/404.js
@@ -3,7 +3,7 @@ const Router = require('express')
 const router = new Router()
 
 router.use((req, res) => {
-  res.status(404).json({message: 'Page not found'})
+  res.status(404).json({message: 'No such endpoint'})
 })
 
 
